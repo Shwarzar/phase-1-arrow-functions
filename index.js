0aef9dd77f9418192159faaa9d25d11389f4db67
Ahmad Mash <wolfmashagbah@hotmail.com>
2023-10-09 10:32:41 +0300

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,13 @@
+const divide = function(a, b) {
+    return a / b
+}
+
+console.log(divide(20, 4));
+
+const square = x => x ** 2
+
+console.log(square(8))
+
+const add = (a, b) => a + b 
+
+console.log(add(10,39))
